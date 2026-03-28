15ca8838e345798d2ac5fe7741280270ef1aa9ce
Spectrall <105946758+Spectrall368@users.noreply.github.com>
2026-03-28 16:46:03 +0100

    Fix model

--- src/test/resources/entitymodel-mojmap-1.17.x.java
+++ src/test/resources/entitymodel-mojmap-1.17.x.java
@@ -2,7 +2,6 @@
 // Exported for Minecraft version 1.17 with Mojang mappings
 // Paste this class into your mod and generate all required imports
 
-
 public class CustomJavaModel<T extends Entity> extends EntityModel<T> {
 	// This layer location should be baked with EntityRendererProvider.Context in the entity renderer and passed into this model's constructor
 	public static final ModelLayerLocation LAYER_LOCATION = new ModelLayerLocation(new ResourceLocation("modid", "custom_java_model"), "main");
--- src/test/resources/entitymodel-mojmap-legacy.java
+++ src/test/resources/entitymodel-mojmap-legacy.java
@@ -1,71 +1,52 @@
-/*
- * MCreator (https://mcreator.net/)
- * Copyright (C) 2012-2020, Pylo
- * Copyright (C) 2020-2025, Pylo, opensource contributors
- *
- * This program is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with this program.  If not, see <https://www.gnu.org/licenses/>.
- */
-
 // Made with Blockbench 5.0.7
 // Exported for Minecraft version 1.14 with MCP mappings
 // Paste this class into your mod and generate all required imports
 
 public class CustomJavaModel extends EntityModel {
-	private final RendererModel Head;
-	private final RendererModel Body;
-	private final RendererModel RightArm;
-	private final RendererModel LeftArm;
-	private final RendererModel RightLeg;
-	private final RendererModel LeftLeg;
+	private final ModelRenderer body;
+	private final ModelRenderer head;
+	private final ModelRenderer leg0;
+	private final ModelRenderer leg1;
+	private final ModelRenderer leg2;
+	private final ModelRenderer leg3;
 
 	public CustomJavaModel() {
 		textureWidth = 64;
-		textureHeight = 64;
+		textureHeight = 32;
 
-		Head = new RendererModel(this);
-		Head.setRotationPoint(0.0F, 0.0F, 0.0F);
-		Head.cubeList.add(new ModelBox(Head, 0, 0, -4.0F, -8.0F, -4.0F, 8, 8, 8, 0.0F, false));
+		body = new ModelRenderer(this);
+		body.setRotationPoint(0.0F, 24.0F, 0.0F);
+		body.cubeList.add(new ModelBox(body, 16, 16, -4.0F, -18.0F, -2.0F, 8, 12, 4, 0.0F, false));
 
-		Body = new RendererModel(this);
-		Body.setRotationPoint(0.0F, 0.0F, 0.0F);
-		Body.cubeList.add(new ModelBox(Body, 16, 16, -4.0F, 0.0F, -2.0F, 8, 12, 4, 0.0F, false));
+		head = new ModelRenderer(this);
+		head.setRotationPoint(0.0F, 6.0F, 0.0F);
+		head.cubeList.add(new ModelBox(head, 0, 0, -4.0F, -8.0F, -4.0F, 8, 8, 8, 0.0F, false));
 
-		RightArm = new RendererModel(this);
-		RightArm.setRotationPoint(-5.0F, 2.0F, 0.0F);
-		RightArm.cubeList.add(new ModelBox(RightArm, 40, 16, -3.0F, -2.0F, -2.0F, 4, 12, 4, 0.0F, false));
+		leg0 = new ModelRenderer(this);
+		leg0.setRotationPoint(2.0F, 18.0F, 4.0F);
+		leg0.cubeList.add(new ModelBox(leg0, 0, 16, -2.0F, 0.0F, -2.0F, 4, 6, 4, 0.0F, false));
 
-		LeftArm = new RendererModel(this);
-		LeftArm.setRotationPoint(5.0F, 2.0F, 0.0F);
-		LeftArm.cubeList.add(new ModelBox(LeftArm, 32, 48, -1.0F, -2.0F, -2.0F, 4, 12, 4, 0.0F, false));
+		leg1 = new ModelRenderer(this);
+		leg1.setRotationPoint(-2.0F, 18.0F, 4.0F);
+		leg1.cubeList.add(new ModelBox(leg1, 0, 16, -2.0F, 0.0F, -2.0F, 4, 6, 4, 0.0F, false));
 
-		RightLeg = new RendererModel(this);
-		RightLeg.setRotationPoint(-1.9F, 12.0F, 0.0F);
-		RightLeg.cubeList.add(new ModelBox(RightLeg, 0, 16, -2.0F, 0.0F, -2.0F, 4, 12, 4, 0.0F, false));
+		leg2 = new ModelRenderer(this);
+		leg2.setRotationPoint(2.0F, 18.0F, -4.0F);
+		leg2.cubeList.add(new ModelBox(leg2, 0, 16, -2.0F, 0.0F, -2.0F, 4, 6, 4, 0.0F, false));
 
-		LeftLeg = new RendererModel(this);
-		LeftLeg.setRotationPoint(1.9F, 12.0F, 0.0F);
-		LeftLeg.cubeList.add(new ModelBox(LeftLeg, 16, 48, -2.0F, 0.0F, -2.0F, 4, 12, 4, 0.0F, false));
+		leg3 = new ModelRenderer(this);
+		leg3.setRotationPoint(-2.0F, 18.0F, -4.0F);
+		leg3.cubeList.add(new ModelBox(leg3, 0, 16, -2.0F, 0.0F, -2.0F, 4, 6, 4, 0.0F, false));
 	}
 
 	@Override
 	public void render(Entity entity, float f, float f1, float f2, float f3, float f4, float f5) {
-		Head.render(f5);
-		Body.render(f5);
-		RightArm.render(f5);
-		LeftArm.render(f5);
-		RightLeg.render(f5);
-		LeftLeg.render(f5);
+		body.render(f5);
+		head.render(f5);
+		leg0.render(f5);
+		leg1.render(f5);
+		leg2.render(f5);
+		leg3.render(f5);
 	}
 
 	public void setRotationAngle(RendererModel modelRenderer, float x, float y, float z) {
